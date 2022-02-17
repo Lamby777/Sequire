130526152fb2890a3ca66b8a9bad4a564a778303
Dex <35111165+Lamby777@users.noreply.github.com>
2022-02-17 19:14:36 +0000

    EJS stuff

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log(`Hello from Node.js ${process.version}!`);
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.app = void 0;
+const PORT = 3000;
+const express_1 = __importDefault(require("express"));
+exports.app = (0, express_1.default)();
+exports.app.set("view engine", "ejs");
+exports.app.use(express_1.default.json());
+exports.app.get("/", (req, res) => {
+    console.log("amogus");
+    res.render("main");
+});
+exports.app.listen(PORT, () => { console.log("Listening on port " + PORT); });
--- index.ts
+++ index.ts
@@ -1,2 +1,21 @@
-// among us
-console.log(`Hello from Node.js ${process.version}!`);
+// Hello human
+"use strict";
+
+const PORT = 3000;
+
+// Imports
+import Express		from "express";
+import ejs			from "ejs";
+import fs			from "fs";
+
+// Start Express server for peers to use
+export const app = Express();
+app.set("view engine", "ejs");
+app.use(Express.json());
+
+app.get("/", (req, res) => {
+	console.log("amogus");
+	res.render("main");
+});
+
+app.listen(PORT, () => { console.log("Listening on port "+ PORT); });
